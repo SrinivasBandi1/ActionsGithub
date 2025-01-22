b809c5407bd509bf3bdfd3c29f9e38cf8125c05f
SrinivasBandi1 <134129477+SrinivasBandi1@users.noreply.github.com>
2025-01-22 23:13:46 +0530

    Update DriverSetupPage.java

--- src/test/java/com/utkarshbank/tests/DriverSetupPage.java
+++ src/test/java/com/utkarshbank/tests/DriverSetupPage.java
@@ -23,8 +23,6 @@ public class DriverSetupPage {
 		
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
-				logger.info("S*************************************** in chrome options ***********************************************d");
-
 options.addArguments("--headless"); // Required for CI
 options.addArguments("--no-sandbox"); // Bypass the sandbox
 options.addArguments("--disable-dev-shm-usage"); // Prevent shared memory issues
